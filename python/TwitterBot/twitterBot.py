a42dd53de28dbc731ec8baea76563ef68f4c3f60
ed1868 <ruizeduardo21@gmail.com>
2023-03-10 15:18:09 -0500

    added functions

--- python/TwitterBot/twitterBot.py
+++ python/TwitterBot/twitterBot.py
@@ -12,6 +12,41 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+
+
+# Define the event date and time
+event_date = datetime.datetime(2023, 3, 17, 12, 0, 0) # year, month, day, hour, minute, second
+
+# Define the message to be tweeted
+message = "Reminder: Our event is happening in 2 hours! Join us at the venue."
+
+# Define the charity donation link
+donation_link = "https://example.com/donate"
+
+# Set the time interval for checking the event time
+interval = 60 # in seconds
+
+while True:
+    # Get the current date and time
+    now = datetime.datetime.now()
+
+    # Calculate the time remaining until the event
+    time_remaining = event_date - now
+
+    # Check if the event is happening in less than 2 hours
+    if time_remaining <= datetime.timedelta(hours=2):
+        # Tweet the reminder message and donation link
+        api.update_status(f"{message} Donate to our charity here: {donation_link}")
+        print("Tweeted:", message)
+        print("Donation link:", donation_link)
+        break
+
+    # Wait for the specified interval before checking the time again
+    time.sleep(interval)
+
+
+
+    
 # Define a function to handle incoming mentions
 def handle_mentions():
     # Get the 20 most recent mentions
